457b65f6ddc5758256b8ccd05e5313d787e43ae5
Ives Chiba <ivesjundi@gmail.com>
2024-11-12 20:27:02 -0300

    chore :wrench: swagger config

---
+++ src/main/java/powerup/v1/infra/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package powerup.v1.infra;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().
+                info(new Info().
+                        title("PowerUp").
+                        version("v1").
+                        description("Ellie de Oliveira\n" +
+                                "\n" +
+                                "RM: 552824\n" +
+                                "\n" +
+                                "Ives Jundi Chiba\n" +
+                                "\n" +
+                                "RM: 553243\n" +
+                                "\n" +
+                                "Nathalia Comeron Freire\n" +
+                                "\n" +
+                                "RM: 553233").
+                        license(new License().
+                                name("PowerUp").
+                                url("https://www.fiap.com.br/")));
+    }
+}
