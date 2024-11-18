879d4752c72189d364b04d24bf07599be7a18edc
IvesJc-Evo <ives.chiba@evosystems.com.br>
2024-11-18 19:55:11 -0300

    feat :sparkles: response DTO for RecompensaTipo entity

--- src/main/java/powerup/v1/usecases/RecompensaTipoService.java
+++ src/main/java/powerup/v1/usecases/RecompensaTipoService.java
@@ -1,14 +1,15 @@
 package powerup.v1.usecases;
 
 import powerup.v1.dtos.request.RecompensaTipoRequestDto;
+import powerup.v1.dtos.response.RecompensaTipoResponseDto;
 import powerup.v1.entities.RecompensaTipo;
 
 import java.util.List;
 
 public interface RecompensaTipoService {
-    RecompensaTipoRequestDto create(RecompensaTipo recompensaTipo);
+    RecompensaTipoRequestDto create(RecompensaTipoResponseDto recompensaTipo);
     List<RecompensaTipoRequestDto> getAll();
     RecompensaTipoRequestDto getById(Integer id);
-    RecompensaTipoRequestDto update(Integer id, RecompensaTipo recompensaTipo);
+    RecompensaTipoRequestDto update(Integer id, RecompensaTipoResponseDto recompensaTipo);
     void delete(Integer id);
 }
