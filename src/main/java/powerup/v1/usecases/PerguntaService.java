90d55f3e4a42153a25c38aa83ef15673ca9992e6
IvesJc-Evo <ives.chiba@evosystems.com.br>
2024-11-18 19:30:07 -0300

    feat :sparkles: response DTO for Pergunta entity

--- src/main/java/powerup/v1/usecases/PerguntaService.java
+++ src/main/java/powerup/v1/usecases/PerguntaService.java
@@ -1,14 +1,15 @@
 package powerup.v1.usecases;
 
 import powerup.v1.dtos.request.PerguntaRequestDto;
+import powerup.v1.dtos.response.PerguntaResponseDto;
 import powerup.v1.entities.Pergunta;
 
 import java.util.List;
 
 public interface PerguntaService {
-    PerguntaRequestDto create(Pergunta pergunta);
+    PerguntaRequestDto create(PerguntaResponseDto pergunta);
     List<PerguntaRequestDto> getAll();
     PerguntaRequestDto getById(Integer id);
-    PerguntaRequestDto update(Integer id, Pergunta pergunta);
+    PerguntaRequestDto update(Integer id, PerguntaResponseDto pergunta);
     void delete(Integer id);
 }
