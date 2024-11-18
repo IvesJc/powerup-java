a731b0b491a2a15e559f68e48e251b3a03c3fe78
IvesJc-Evo <ives.chiba@evosystems.com.br>
2024-11-18 19:44:09 -0300

    feat :sparkles: response DTO for RecompensaConfig entity

--- src/main/java/powerup/v1/usecases/RecompensaConfigService.java
+++ src/main/java/powerup/v1/usecases/RecompensaConfigService.java
@@ -1,14 +1,15 @@
 package powerup.v1.usecases;
 
 import powerup.v1.dtos.request.RecompensaConfigRequestDto;
+import powerup.v1.dtos.response.RecompensaConfigResponseDto;
 import powerup.v1.entities.RecompensaConfig;
 
 import java.util.List;
 
 public interface RecompensaConfigService {
-    RecompensaConfigRequestDto create(RecompensaConfig recompensaConfig);
+    RecompensaConfigRequestDto create(RecompensaConfigResponseDto recompensaConfig);
     List<RecompensaConfigRequestDto> getAll();
     RecompensaConfigRequestDto getById(Integer id);
-    RecompensaConfigRequestDto update(Integer id, RecompensaConfig recompensaConfig);
+    RecompensaConfigRequestDto update(Integer id, RecompensaConfigResponseDto recompensaConfig);
     void delete(Integer id);
 }
--- src/main/java/powerup/v1/usecases/impl/RecompensaConfigServiceImpl.java
+++ src/main/java/powerup/v1/usecases/impl/RecompensaConfigServiceImpl.java
@@ -3,8 +3,11 @@ package powerup.v1.usecases.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import powerup.v1.dtos.request.RecompensaConfigRequestDto;
+import powerup.v1.dtos.response.RecompensaConfigResponseDto;
 import powerup.v1.entities.RecompensaConfig;
+import powerup.v1.entities.RecompensaTipo;
 import powerup.v1.repositories.RecompensaConfigRepository;
+import powerup.v1.repositories.RecompensaTipoRepository;
 import powerup.v1.usecases.RecompensaConfigService;
 import powerup.v1.usecases.exception.IdNotFoundException;
 
@@ -15,10 +18,18 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RecompensaConfigServiceImpl implements RecompensaConfigService {
     private final RecompensaConfigRepository recompensaConfigRepository;
+    private final RecompensaTipoRepository recompensaTipoRepository;
 
     @Override
-    public RecompensaConfigRequestDto create(RecompensaConfig recompensaConfig) {
-        RecompensaConfig savedEntity = recompensaConfigRepository.save(recompensaConfig);
+    public RecompensaConfigRequestDto create(RecompensaConfigResponseDto recompensaConfig) {
+
+        RecompensaTipo recompensaTipo = recompensaTipoRepository.findById(recompensaConfig.recompensaTipo()).orElseThrow(() -> new IdNotFoundException("Ranking not found"));
+
+        RecompensaConfig savedEntity = RecompensaConfig.builder()
+                .nome(recompensaConfig.nome())
+                .custoPontos(recompensaConfig.custoPontos())
+                .recompensaTipo(recompensaTipo).build();
+        recompensaConfigRepository.save(savedEntity);
         return mapToDTO(savedEntity);
     }
 
@@ -38,13 +49,18 @@ public class RecompensaConfigServiceImpl implements RecompensaConfigService {
     }
 
     @Override
-    public RecompensaConfigRequestDto update(Integer id, RecompensaConfig recompensaConfig) {
-        if (!recompensaConfigRepository.existsById(id)) {
-            throw new IdNotFoundException("RecompensaConfig not found with id: " + id);
-        }
-        recompensaConfig.setId(id);
-        RecompensaConfig updatedEntity = recompensaConfigRepository.save(recompensaConfig);
-        return mapToDTO(updatedEntity);
+    public RecompensaConfigRequestDto update(Integer id, RecompensaConfigResponseDto recompensaConfig) {
+
+        RecompensaTipo recompensaTipo = recompensaTipoRepository.findById(recompensaConfig.recompensaTipo()).orElseThrow(() -> new IdNotFoundException("Ranking not found"));
+
+        RecompensaConfig updateRecompensaConfig = recompensaConfigRepository.findById(id).orElseThrow(() -> new IdNotFoundException("Recompensa Config not found with this id: "+id));
+
+        updateRecompensaConfig.setNome(recompensaConfig.nome());
+        updateRecompensaConfig.setCustoPontos(recompensaConfig.custoPontos());
+        updateRecompensaConfig.setRecompensaTipo(recompensaTipo);
+
+        recompensaConfigRepository.save(updateRecompensaConfig);
+        return mapToDTO(updateRecompensaConfig);
     }
 
     @Override
