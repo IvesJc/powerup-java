a6a86f120ebe01f91881c4349e2670877e96e170
IvesJc-Evo <ives.chiba@evosystems.com.br>
2024-11-18 19:15:08 -0300

    feat :sparkles: response DTO for Link entity

--- src/main/java/powerup/v1/usecases/LinkService.java
+++ src/main/java/powerup/v1/usecases/LinkService.java
@@ -1,15 +1,16 @@
 package powerup.v1.usecases;
 
 import powerup.v1.dtos.request.LinkRequestDto;
+import powerup.v1.dtos.response.LinkResponseDto;
 import powerup.v1.entities.Link;
 
 import java.util.List;
 
 public interface LinkService {
-    LinkRequestDto create(Link link);
+    LinkRequestDto create(LinkResponseDto link);
     List<LinkRequestDto> getAll();
     LinkRequestDto getById(Integer id);
-    LinkRequestDto update(Integer id, Link link);
+    LinkRequestDto update(Integer id, LinkResponseDto link);
     void delete(Integer id);
     
 }
