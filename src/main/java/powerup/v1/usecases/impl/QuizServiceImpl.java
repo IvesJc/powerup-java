0bbbc8a5609e38789c1f2a7f488a2b4321ab55d5
IvesJc-Evo <ives.chiba@evosystems.com.br>
2024-11-18 19:36:44 -0300

    feat :sparkles: response DTO for Quiz entity

--- src/main/java/powerup/v1/usecases/impl/QuizServiceImpl.java
+++ src/main/java/powerup/v1/usecases/impl/QuizServiceImpl.java
@@ -3,7 +3,10 @@ package powerup.v1.usecases.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import powerup.v1.dtos.request.QuizRequestDto;
+import powerup.v1.dtos.response.QuizResponseDto;
+import powerup.v1.entities.ModuloEducativo;
 import powerup.v1.entities.Quiz;
+import powerup.v1.repositories.ModuloEducativoRepository;
 import powerup.v1.repositories.QuizRepository;
 import powerup.v1.usecases.QuizService;
 import powerup.v1.usecases.exception.IdNotFoundException;
@@ -15,10 +18,21 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class QuizServiceImpl implements QuizService {
     private final QuizRepository quizRepository;
+    private final ModuloEducativoRepository moduloEducativoRepository;
 
     @Override
-    public QuizRequestDto create(Quiz quiz) {
-        Quiz savedEntity = quizRepository.save(quiz);
+    public QuizRequestDto create(QuizResponseDto quiz) {
+
+        ModuloEducativo moduloEducativo = moduloEducativoRepository.findById(quiz.moduloEducativo()).orElseThrow(() -> new IdNotFoundException("ModuloEducativo not found"));
+
+        Quiz savedEntity = Quiz.builder()
+                .nome(quiz.nome())
+                .descricao(quiz.descricao())
+                .categoria(quiz.categoria())
+                .notaMinima(quiz.notaMinima())
+                .moduloEducativo(moduloEducativo).build();
+
+        quizRepository.save(savedEntity);
         return mapToDTO(savedEntity);
     }
 
@@ -38,12 +52,18 @@ public class QuizServiceImpl implements QuizService {
     }
 
     @Override
-    public QuizRequestDto update(Integer id, Quiz quiz) {
-        if (!quizRepository.existsById(id)) {
-            throw new IdNotFoundException("Quiz not found with id: " + id);
-        }
-        quiz.setId(id);
-        Quiz updatedEntity = quizRepository.save(quiz);
+    public QuizRequestDto update(Integer id, QuizResponseDto quiz) {
+        ModuloEducativo moduloEducativo = moduloEducativoRepository.findById(quiz.moduloEducativo()).orElseThrow(() -> new IdNotFoundException("ModuloEducativo not found"));
+
+        Quiz updatedEntity = quizRepository.findById(id).orElseThrow(() -> new IdNotFoundException("Quiz not found with id: " + id));
+
+        updatedEntity.setNome(quiz.nome());
+        updatedEntity.setDescricao(quiz.descricao());
+        updatedEntity.setCategoria(quiz.categoria());
+        updatedEntity.setNotaMinima(quiz.notaMinima());
+        updatedEntity.setModuloEducativo(moduloEducativo);
+
+        quizRepository.save(updatedEntity);
         return mapToDTO(updatedEntity);
     }
 
