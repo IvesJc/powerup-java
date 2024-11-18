f25f7f22a39e0885e907ac7d231aa5b06524fab4
IvesJc-Evo <ives.chiba@evosystems.com.br>
2024-11-18 19:12:27 -0300

    feat :sparkles: response DTO for EmblemaTipo entity

--- src/main/java/powerup/v1/usecases/EmblemaTipoService.java
+++ src/main/java/powerup/v1/usecases/EmblemaTipoService.java
@@ -1,14 +1,15 @@
 package powerup.v1.usecases;
 
 import powerup.v1.dtos.request.EmblemaTipoRequestDto;
+import powerup.v1.dtos.response.EmblemaTipoResponseDto;
 import powerup.v1.entities.EmblemaTipo;
 
 import java.util.List;
 
 public interface EmblemaTipoService {
-    EmblemaTipoRequestDto create(EmblemaTipo emblemaTipo);
+    EmblemaTipoRequestDto create(EmblemaTipoResponseDto emblemaTipo);
     List<EmblemaTipoRequestDto> getAll();
     EmblemaTipoRequestDto getById(Integer id);
-    EmblemaTipoRequestDto update(Integer id, EmblemaTipo emblemaTipo);
+    EmblemaTipoRequestDto update(Integer id, EmblemaTipoResponseDto emblemaTipo);
     void delete(Integer id);
 }
--- src/main/java/powerup/v1/usecases/impl/EmblemaTipoServiceImpl.java
+++ src/main/java/powerup/v1/usecases/impl/EmblemaTipoServiceImpl.java
@@ -3,8 +3,11 @@ package powerup.v1.usecases.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import powerup.v1.dtos.request.EmblemaTipoRequestDto;
+import powerup.v1.dtos.response.EmblemaTipoResponseDto;
 import powerup.v1.entities.EmblemaTipo;
+import powerup.v1.entities.Link;
 import powerup.v1.repositories.EmblemaTipoRepository;
+import powerup.v1.repositories.LinkRepository;
 import powerup.v1.usecases.EmblemaTipoService;
 import powerup.v1.usecases.exception.IdNotFoundException;
 
@@ -15,10 +18,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class EmblemaTipoServiceImpl implements EmblemaTipoService {
     private final EmblemaTipoRepository emblemaTipoRepository;
+    private final LinkRepository linkRepository;
 
     @Override
-    public EmblemaTipoRequestDto create(EmblemaTipo emblemaTipo) {
-        EmblemaTipo savedEntity = emblemaTipoRepository.save(emblemaTipo);
+    public EmblemaTipoRequestDto create(EmblemaTipoResponseDto emblemaTipo) {
+
+        Link link = linkRepository.findById(emblemaTipo.imageLink()).orElseThrow(() -> new IdNotFoundException("EmblemaTipo not found with id: " + emblemaTipo.imageLink()));
+
+        EmblemaTipo savedEntity = EmblemaTipo.builder()
+                .nome(emblemaTipo.nome())
+                .imageLinkId(link).build();
+
+        emblemaTipoRepository.save(savedEntity);
+
         return mapToDTO(savedEntity);
     }
 
@@ -38,13 +50,17 @@ public class EmblemaTipoServiceImpl implements EmblemaTipoService {
     }
 
     @Override
-    public EmblemaTipoRequestDto update(Integer id, EmblemaTipo emblemaTipo) {
-        if (!emblemaTipoRepository.existsById(id)) {
-            throw new IdNotFoundException("EmblemaTipo not found with id: " + id);
-        }
-        emblemaTipo.setId(id);
-        EmblemaTipo updatedEntity = emblemaTipoRepository.save(emblemaTipo);
-        return mapToDTO(updatedEntity);
+    public EmblemaTipoRequestDto update(Integer id, EmblemaTipoResponseDto emblemaTipo) {
+
+        Link link = linkRepository.findById(emblemaTipo.imageLink()).orElseThrow(() -> new IdNotFoundException("EmblemaTipo not found with id: " + emblemaTipo.imageLink()));
+
+        EmblemaTipo updateEmblemaTipo = emblemaTipoRepository.findById(id).orElseThrow(() -> new IdNotFoundException("EmblemaTipo not found with id: " + id));
+
+        updateEmblemaTipo.setNome(emblemaTipo.nome());
+        updateEmblemaTipo.setImageLinkId(link);
+
+        emblemaTipoRepository.save(updateEmblemaTipo);
+        return mapToDTO(updateEmblemaTipo);
     }
 
     @Override
